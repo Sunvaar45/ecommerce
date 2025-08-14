3983f18176592b0a335a0843ccc78e917ccaa99a
Sunvaar45 <is.kalay1@gmail.com>
2025-08-14 14:41:08 +0300

    Refactor Products model to include HasFactory trait and define fillable fields

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'products';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'discount_price',
+        'image_url',
+        'category_id',
+        'status',
+    ];
 }
