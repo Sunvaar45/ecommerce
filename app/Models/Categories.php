bf505541b33104676eb1a7a197a89410bf4bae1d
Sunvaar45 <is.kalay1@gmail.com>
2025-08-14 11:06:06 +0300

    Add Categories model with relationships and fillable attributes

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Categories extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'category_name',
+        'category_url',
+        'parent_id',
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo(Categories::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Categories::class, 'parent_id');
+    }
 }
