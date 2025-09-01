00c0f4e2f6c804b4626c707503ab74a10eb9b3aa
Sunvaar45 <is.kalay1@gmail.com>
2025-09-01 13:42:02 +0300

    Make image_url field nullable in product_images migration

--- database/migrations/2025_08_18_124748_create_product_images_table.php
+++ database/migrations/2025_08_18_124748_create_product_images_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('product_images', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('product_id');
-            $table->string('image_url');
+            $table->string('image_url')->nullable();
             $table->string('image_alt')->nullable();
             $table->integer('sort_order')->default(0);
             $table->boolean('is_main')->default(false)
